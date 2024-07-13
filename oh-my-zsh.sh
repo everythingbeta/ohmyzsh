4befb23019785a984248939533ba381ef51ae5d8
everythingbeta <145017187+everythingbeta@users.noreply.github.com>
2024-07-13 11:18:50 +0000

    Invalid DateTime

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -68,7 +68,7 @@ mkdir -p "$ZSH_CACHE_DIR/completions"
 (( ${fpath[(Ie)"$ZSH_CACHE_DIR/completions"]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
 
 # Check for updates on initial load...
-source "$ZSH/tools/check_for_upgrade.sh"
+# source "$ZSH/tools/check_for_upgrade.sh" # Commented out to speed up remote integrations
 
 # Initializes Oh My Zsh
 
@@ -111,124 +111,11 @@ if [[ -z "$ZSH_COMPDUMP" ]]; then
 fi
 
 # Construct zcompdump OMZ metadata
-zcompdump_revision="#omz revision: $(builtin cd -q "$ZSH"; git rev-parse HEAD 2>/dev/null)"
-zcompdump_fpath="#omz fpath: $fpath"
+# zcompdump_revision="#omz revision: $(builtin cd -q "$ZSH"; git rev-parse HEAD 2>/dev/null)" # Commented out to speed up remote integrations
+# zcompdump_fpath="#omz fpath: $fpath" # Commented out to speed up remote integrations
 
 # Delete the zcompdump file if OMZ zcompdump metadata changed
-if ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null \
-  || ! command grep -q -Fx "$zcompdump_fpath" "$ZSH_COMPDUMP" 2>/dev/null; then
-  command rm -f "$ZSH_COMPDUMP"
-  zcompdump_refresh=1
-fi
-
-if [[ "$ZSH_DISABLE_COMPFIX" != true ]]; then
-  source "$ZSH/lib/compfix.zsh"
-  # Load only from secure directories
-  compinit -i -d "$ZSH_COMPDUMP"
-  # If completion insecurities exist, warn the user
-  handle_completion_insecurities &|
-else
-  # If the user wants it, load from all found directories
-  compinit -u -d "$ZSH_COMPDUMP"
-fi
-
-# Append zcompdump metadata if missing
-if (( $zcompdump_refresh )) \
-  || ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null; then
-  # Use `tee` in case the $ZSH_COMPDUMP filename is invalid, to silence the error
-  # See https://github.com/ohmyzsh/ohmyzsh/commit/dd1a7269#commitcomment-39003489
-  tee -a "$ZSH_COMPDUMP" &>/dev/null <<EOF
-
-$zcompdump_revision
-$zcompdump_fpath
-EOF
-fi
-unset zcompdump_revision zcompdump_fpath zcompdump_refresh
-
-# zcompile the completion dump file if the .zwc is older or missing.
-if command mkdir "${ZSH_COMPDUMP}.lock" 2>/dev/null; then
-  zrecompile -q -p "$ZSH_COMPDUMP"
-  command rm -rf "$ZSH_COMPDUMP.zwc.old" "${ZSH_COMPDUMP}.lock"
-fi
-
-_omz_source() {
-  local context filepath="$1"
-
-  # Construct zstyle context based on path
-  case "$filepath" in
-  lib/*) context="lib:${filepath:t:r}" ;;         # :t = lib_name.zsh, :r = lib_name
-  plugins/*) context="plugins:${filepath:h:t}" ;; # :h = plugins/plugin_name, :t = plugin_name
-  esac
-
-  local disable_aliases=0
-  zstyle -T ":omz:${context}" aliases || disable_aliases=1
-
-  # Back up alias names prior to sourcing
-  local -A aliases_pre galiases_pre
-  if (( disable_aliases )); then
-    aliases_pre=("${(@kv)aliases}")
-    galiases_pre=("${(@kv)galiases}")
-  fi
-
-  # Source file from $ZSH_CUSTOM if it exists, otherwise from $ZSH
-  if [[ -f "$ZSH_CUSTOM/$filepath" ]]; then
-    source "$ZSH_CUSTOM/$filepath"
-  elif [[ -f "$ZSH/$filepath" ]]; then
-    source "$ZSH/$filepath"
-  fi
-
-  # Unset all aliases that don't appear in the backed up list of aliases
-  if (( disable_aliases )); then
-    if (( #aliases_pre )); then
-      aliases=("${(@kv)aliases_pre}")
-    else
-      (( #aliases )) && unalias "${(@k)aliases}"
-    fi
-    if (( #galiases_pre )); then
-      galiases=("${(@kv)galiases_pre}")
-    else
-      (( #galiases )) && unalias "${(@k)galiases}"
-    fi
-  fi
-}
-
-# Load all of the lib files in ~/oh-my-zsh/lib that end in .zsh
-# TIP: Add files you don't want in git to .gitignore
-for lib_file ("$ZSH"/lib/*.zsh); do
-  _omz_source "lib/${lib_file:t}"
-done
-unset lib_file
-
-# Load all of the plugins that were defined in ~/.zshrc
-for plugin ($plugins); do
-  _omz_source "plugins/$plugin/$plugin.plugin.zsh"
-done
-unset plugin
-
-# Load all of your custom configurations from custom/
-for config_file ("$ZSH_CUSTOM"/*.zsh(N)); do
-  source "$config_file"
-done
-unset config_file
-
-# Load the theme
-is_theme() {
-  local base_dir=$1
-  local name=$2
-  builtin test -f $base_dir/$name.zsh-theme
-}
-
-if [[ -n "$ZSH_THEME" ]]; then
-  if is_theme "$ZSH_CUSTOM" "$ZSH_THEME"; then
-    source "$ZSH_CUSTOM/$ZSH_THEME.zsh-theme"
-  elif is_theme "$ZSH_CUSTOM/themes" "$ZSH_THEME"; then
-    source "$ZSH_CUSTOM/themes/$ZSH_THEME.zsh-theme"
-  elif is_theme "$ZSH/themes" "$ZSH_THEME"; then
-    source "$ZSH/themes/$ZSH_THEME.zsh-theme"
-  else
-    echo "[oh-my-zsh] theme '$ZSH_THEME' not found"
-  fi
-fi
-
-# set completion colors to be the same as `ls`, after theme has been loaded
-[[ -z "$LS_COLORS" ]] || zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+# if ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null \
+#   || ! command grep -q -Fx "$zcompdump_fpath" "$ZSH_COMPDUMP" 2>/dev/null; then
+#   command rm -f "$ZSH_COMPDUMP"
+#   zcompdump_refresh=1
